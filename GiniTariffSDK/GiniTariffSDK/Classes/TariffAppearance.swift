ab6768ab65ed740e3cfe5cb5a51d75a3c70c6ec1
Nikola <n.sobadjiev@gini.net>
2017-05-30 11:29:49 +0200

    Made TariffAppearance a public class

--- GiniTariffSDK/GiniTariffSDK/Classes/TariffAppearance.swift
+++ GiniTariffSDK/GiniTariffSDK/Classes/TariffAppearance.swift
@@ -14,7 +14,7 @@ import UIKit
  * The TariffAppearance class contains resources, texts and colors used for customizing the
  * SDKs UI to match the hosting app it is used with.
  */
-class TariffAppearance {
+public class TariffAppearance {
     
     /**
      * The positive color is a color that will show up at many places throughout the SDK.
