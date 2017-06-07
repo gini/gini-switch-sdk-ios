fd680018156c1859535bde9d37b567ec500af254
Nikola <n.sobadjiev@gini.net>
2017-06-07 18:07:46 +0200

    Removed setup and teardown methods in UserTests

--- GiniTariffSDK/Example/Tests/UserTests.swift
+++ GiniTariffSDK/Example/Tests/UserTests.swift
@@ -13,16 +13,6 @@ class UserTests: XCTestCase {
     
     var user = User()
     
-    override func setUp() {
-        super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-    
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
-    }
-    
     func testHasPassword() {
         user.password = "test"
         XCTAssertNotNil(user.password, "User should have an password")
