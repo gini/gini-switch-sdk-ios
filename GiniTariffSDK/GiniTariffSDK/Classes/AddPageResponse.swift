76448c1edf97a65b9617c3a438eec9ee91f405fa
Nikola <n.sobadjiev@gini.net>
2017-06-09 15:39:38 +0200

    Added Data model classes for the extraction service responses (not yet integrated)

---
+++ GiniTariffSDK/GiniTariffSDK/Classes/AddPageResponse.swift
@@ -0,0 +1,31 @@
+//
+//  AddPageResponse.swift
+//  Pods
+//
+//  Created by Nikola Sobadjiev on 09.06.17.
+//
+//
+
+
+class AddPageResponse: BaseApiResponse {
+
+    static let statusMapping = ["processing": ScanPageStatus.uploading, "processed": .analysed, "failed": .failed]
+    
+    let status:String?  // TODO: the status should be mandatory
+    
+    override init(dict: JSONDictionary) {
+        status = dict["status"] as? String
+        super.init(dict: dict)
+    }
+    
+    var pageStatus:ScanPageStatus {
+        if let status = self.status,
+            let pageStatus = AddPageResponse.statusMapping[status] {
+            return pageStatus
+        }
+        else {
+            return .none
+        }
+    }
+
+}
---
+++ GiniTariffSDK/GiniTariffSDK/Classes/BaseApiResponse.swift
@@ -0,0 +1,32 @@
+//
+//  BaseApiResponse.swift
+//  Pods
+//
+//  Created by Nikola Sobadjiev on 09.06.17.
+//
+//
+
+/*
+ * BaseApiResponse is a base class that contains JSON elements present in all responses
+ * received from the Switch API.
+ *
+ * Other responses should inherit from this class and add their additional elements
+ */
+class BaseApiResponse {
+
+    let href:String?
+    
+    init(href:String) {
+        self.href = href
+    }
+    
+    init(dict:JSONDictionary) {
+        if let links = dict["_links"] as? JSONDictionary,
+            let selfLink = links["self"] as? JSONDictionary {
+            href = selfLink["href"] as? String
+        }
+        else {
+            href = nil
+        }
+    }
+}
