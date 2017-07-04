24a6727c1620a872663768bae862e11628d71882
Nikola <n.sobadjiev@gini.net>
2017-07-04 11:53:25 +0200

    Removed autogenerated code from TariffOnboardingTest

--- GiniTariffSDK/Example/Tests/TariffOnboardingTests.swift
+++ GiniTariffSDK/Example/Tests/TariffOnboardingTests.swift
@@ -14,16 +14,6 @@ class TariffOnboardingTests: XCTestCase {
     let testImage = UIImage()
     let testText = "Go onboard yourself"
     
-    override func setUp() {
-        super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-    
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
-    }
-    
     func testCreatingOnboardingPage() {
         let page = testPage()
         XCTAssertEqual(page.image, testImage, "OnboardingPage needs to have an image property")
