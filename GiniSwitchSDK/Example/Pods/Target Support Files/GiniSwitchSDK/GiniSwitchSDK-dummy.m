fa7a3a79e8518ea990b3a0069337e13be0d0634e
Nikola <n.sobadjiev@gini.net>
2017-08-23 11:27:33 +0200

    Added jazzy documentation for 0.9.1

--- GiniSwitchSDK/Example/Pods/Target Support Files/GiniSwitchSDK/GiniSwitchSDK-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_GiniSwitchSDK : NSObject
-@end
-@implementation PodsDummy_GiniSwitchSDK
-@end
--- GiniSwitchSDK/Example/Pods/Target Support Files/Pods-GiniSwitchSDK_Example/Pods-GiniSwitchSDK_Example-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_GiniSwitchSDK_Example : NSObject
-@end
-@implementation PodsDummy_Pods_GiniSwitchSDK_Example
-@end
